b4adba6c95cb43a94ed4798741db7473a187f9c5
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-04-30 22:46:55 -0600

    test

--- test.R
+++ test.R
@@ -24,14 +24,12 @@ AFDM <- function(data,
   
   . <- data %>%
   filter(Treatment != "Control") %>%
-  mutate_(IDWc = lazyeval::interp (~a/b, a=as.name(InitDryW), b=as.name("meanControl")))%>% # Corrects dry mass (laboratory) for mass lost from handling
+  mutate_(IDWc = lazyeval::interp (~a*b, a=as.name(InitDryW), b=as.name("meanControl")))%>% # Corrects dry mass (laboratory) for mass lost from handling
   mutate_(AFDMFraction = lazyeval::interp (~(a-b)/a, a= as.name (FractIntW), b= as.name (FractFinW))) %>%
     mutate_(AFDM_Initial = lazyeval::interp (~a*b, a= as.name("IDWc"), b= as.name("AFDMFraction"))) %>% # AFDM in the initial sample
     mutate_(AFDM_Final = lazyeval::interp (~a*b, a= as.name(FinalDryW), b= as.name("AFDMFraction")))%>%
     mutate_(AFDMRemaining = lazyeval::interp (~(a/b)*100, a= as.name("AFDM_Final"), b= as.name("AFDM_Initial")))
   return(.)
-}  
-  
   AFDM1 <- select_(.,Day, Replicate, Treatment, "AFDMRemaining")
   AFDM2 <- arrange(AFDM1, Treatment, Replicate)
   
