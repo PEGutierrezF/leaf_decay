4f21763dc446aea3ad586182f3b006e3c17bb547
Pablo E. Gutierrez Fonseca <pabloe.gutierrezfonseca@gmail.com>
2020-07-01 17:11:49 -0600

    Question for mutate

--- Question.R
+++ Question.R
@@ -1,9 +1,61 @@
 
+control <- data.frame(Day=c(0,0,0,0,0,0),
+                  Replica=c(1,1,1,1,1,1),
+                  Initial_Dry_Weight=c(5.010,5.010,5.010,5.010,5.010,5.000),
+                  Final_Dry_Weight=c(4.990,4.940,4.840,4.820,4.960,4.970),
+                  InitiaFraction=c(1.1071,1.1964,1.0647,1.0005,1.0453,1.1212),
+                  FinalFraction=c(0.3858,0.3504,0.4248,0.3333,0.3417,0.3467),
+                  Treatment=c("Control","Control","Control","Control","Control","Control"))
+control
+
+sites <-data.frame(Day=c(2,4,8,16,32,44),
+                   Replica=c(1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3),
+                   Initial_Dry_Weight=c(5.000,5.000,5.000,5.000,5.01,5.000,5.000,5.000,
+                                        5.000,5.000,5.000,5.01,5.01,5.01,5.000,5.000,5.000,5.000),
+                   Final_Dry_Weight=c(4.65,4.63,4.67,4.64,4.37,4.37,4.17,3.72,4.12,4,3.99,3.64,
+                                      4.26,3.3,3.47,3.7,3.75,3.3),
+                InitiaFraction=c(1.0081,1.0972,1.1307,1.0898,1.075,1.0295,1.0956,1.042,1.0876,
+                 1.006,1.1052,1.0922,1.0472,1.0843,1.0177,1.0143,1.1112,1.0061),
+                FinalFraction=c(0.3229,0.3605,0.3304,0.3489,0.3181,0.2948,0.4098,0.3762,0.3787,
+                0.3345,0.3595,0.3511,0.3921,0.3908,0.3385,0.347,0.3366,0.3318),
+                Treatment=c("CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC",
+                    "CC","CC","CC","CC","CC"))
+
+sites
+
+total <- dplyr::bind_rows(control,sites)
+total
+
+
+manipulation <- function(data,
+                         InitDryW,
+                         FinalDryW,
+                         Treatment,
+                         Difference) {
+  control <- data %>% 
+    filter(Treatment == "Control") %>%
+    select(InitDryW,FinalDryW) %>%
+    mutate_(Difference = lazyeval::interp (~a/b,  a=as.name(FinalDryW),b=as.name(InitDryW)))
+    meanControl <- mean(control$Difference, na.rm = TRUE)
+    return (meanControl)
+}
+manipulation()
+
+
+control <- manipulation(data= total,
+                        InitDryW = "Initial_Dry_Weight",
+                        FinalDryW = "Final_Dry_Weight",
+                        Treatment = "Treatment")
+control
+
+
+
+
 example<- data.frame(Day=c(2,4,8,16,32,44,2,4,8,16,32,44,2,4,8,16,32,44),
           Replicate=c(1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,
                       1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3,
                       1,1,1,1,1,1,2,2,2,2,2,2,3,3,3,3,3,3),
-          Treament=c("CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC",
+          Treatment=c("CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC","CC",
                      "HP","HP","HP","HP","HP","HP","HP","HP","HP","HP","HP","HP","HP","HP","HP","HP","HP","HP",
                      "LL","LL","LL","LL","LL","LL","LL","LL","LL","LL","LL","LL","LL","LL","LL","LL","LL","LL"),
           AFDM=c(94.669342,94.465752,84.897023,81.435993,86.556221,75.328294,94.262162,88.791240,75.735474,81.232403,
